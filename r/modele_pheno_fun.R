36f53b698baa39580a06d9662cb2792b80617800
AntoineM <antoine81518@gmail.com>
2017-07-28 17:42:02 +0200

    Fonctions d'évolution pour le modèle phénoménologique, suivis à représenter graphiquement.

---
+++ r/modele_pheno_fun.R
@@ -0,0 +1,20 @@
+### Première volée de fonctions d'évolution : scalaires ; rat 19, une modalité, un jour. ###
+
+# ADC : pour le moment aucune relation avec les autres modalités. #
+
+succ11_ADC_J0 <- function(p){
+  return()
+}
+
+
+# CBF : même chose que précédemment. #
+
+
+
+
+### Deuxième volée de fonctions : dans un répertoire de benjamin_antoine_labo, utilise des versions vectorisées des fonctions précédentes. ###
+## Prennent en arguments des coordonnées pour le coin inférieur gauche, et la taille du carré de pixels. ##
+
+
+# Dans un premier temps : fonctions qui génèrent et enregistrent éventuellement des dataframe pour les suivis temporels.
+# Dans un deuxième temps : Affichage, comparaisons avec les données répertoriées pour les zones lésées.
